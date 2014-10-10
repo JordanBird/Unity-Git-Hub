03611dc76bad3a55b498f7eeb4b9e8bda06dd367
Jordan Bird <jordanbird-29@hotmail.co.uk>
2014-10-10 10:44:44 +0100

    Added Moving X Ship

---
+++ Asteroids/Assets/AIShipBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIShipBehaviour : MonoBehaviour
+{
+	public float speed = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+	}
+}
