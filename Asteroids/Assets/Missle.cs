59d89ba7d1b6a9c30928e5ebd6a04ab132e3dcec
RealPlay <ashbh@hotmail.co.uk>
2014-10-10 16:59:38 +0100

    Changed big splosion and missiles
    Changed the splosion prefab and added cumtime to missiles

--- Asteroids/Assets/Missle.cs
+++ Asteroids/Assets/Missle.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Missle : MonoBehaviour
 {
 	private float speed = 10;
+	private float cumtime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,11 @@ public class Missle : MonoBehaviour
 	{
 		//transform.Translate (transform.right * Time.deltaTime);
 		rigidbody2D.velocity = transform.right * speed;
+		cumtime += Time.deltaTime;
+		if (cumtime > 10)
+		{
+			Destroy(this.gameObject);
+		} 
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
